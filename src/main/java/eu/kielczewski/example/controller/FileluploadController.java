fab686bb8c6e9ac1f375a1b133b1c1aa2708a005
karthikeyan v veerabathran <kvvchetty@outlook.com>
2020-05-19 08:37:09 -0400

    Create FileluploadController.java
    rest call to upload file data

---
+++ src/main/java/eu/kielczewski/example/controller/FileluploadController.java
@@ -0,0 +1,35 @@
+package com.ryan.practice.rest;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/api")
+public class FileluploadController {
+
+	@PostMapping
+	public ResponseEntity<String> uploadFile(@RequestPart("file") MultipartFile file) {
+		if (null == file.getOriginalFilename()) {
+			return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+		}
+		try {
+			byte[] bytes = file.getBytes();
+			Path path = Paths.get(file.getOriginalFilename());
+			Files.write(path, bytes);
+			System.out.println(path.getFileName());
+		} catch (IOException e) {
+			System.out.println(e.getMessage());
+		}
+		return new ResponseEntity<>("Good Job", HttpStatus.OK);
+	}
+}
